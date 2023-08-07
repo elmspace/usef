5be229f7cdbea6f92e6cc133eeb03c427eda2ab5
Ashkan Dehghan <ash@Ashkans-MacBook-Air.local>
2023-08-07 18:14:14 +0000

    Adding a init file.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 DESCRIPTION = 'Unsupervised Structural Embedding Framework'
 LONG_DESCRIPTION = 'An unsupervised framework for exploring and evaluating node structural embedding of graphs'
 
